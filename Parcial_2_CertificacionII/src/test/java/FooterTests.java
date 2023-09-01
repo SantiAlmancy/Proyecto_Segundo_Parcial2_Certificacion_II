184a34bf443ae10cb9860b9dd811b10bbd859636
Santiago Becerra <santialmancy@gmail.com>
2023-09-01 14:14:47 -0400

    DEVELOP Testcases for footer links created.

---
+++ Parcial_2_CertificacionII/src/test/java/FooterTests.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.HomePage;
+import pages.LoginPage;
+import utilities.DriverManager;
+
+public class FooterTests extends BaseTest
+{
+    @Test
+    public void twitterRedirection()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        HomePage homePage = new HomePage(DriverManager.getDriver().driver);
+        homePage.clickOnTwitterLink();
+
+        DriverManager.switchToNextWindow();
+        String expectedUrl = "https://twitter.com/saucelabs";
+        String newUrl = DriverManager.getCurrentURL();
+        Assertions.assertEquals(expectedUrl, newUrl);
+    }
+
+    @Test
+    public void facebookRedirection()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        HomePage homePage = new HomePage(DriverManager.getDriver().driver);
+        homePage.clickOnFacebookLink();
+
+        DriverManager.switchToNextWindow();
+        String expectedUrl = "https://www.facebook.com/saucelabs";
+        String newUrl = DriverManager.getCurrentURL();
+        Assertions.assertEquals(expectedUrl, newUrl);
+    }
+
+    @Test
+    public void linkedinRedirection()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        HomePage homePage = new HomePage(DriverManager.getDriver().driver);
+        homePage.clickOnLinkedinLink();
+
+        DriverManager.switchToNextWindow();
+        String expectedUrl = "https://www.linkedin.com/company/sauce-labs/";
+        String newUrl = DriverManager.getCurrentURL();
+        Assertions.assertEquals(expectedUrl, newUrl);
+    }
+}
