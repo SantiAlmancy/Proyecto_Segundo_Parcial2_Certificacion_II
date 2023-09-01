f104618fb225bdc5b989a397b358280c146ca2e1
Santiago Becerra <santialmancy@gmail.com>
2023-09-01 14:18:30 -0400

    DEVELOP Testcases for menu created (About, Logout, Reset app state).

---
+++ Parcial_2_CertificacionII/src/test/java/MenuTests.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.MenuPage;
+import utilities.DriverManager;
+
+public class MenuTests extends BaseTest
+{
+    @Test
+    public void aboutSwagLabs()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        MenuPage menuPage = new MenuPage(DriverManager.getDriver().driver);
+        menuPage.clickOnMenuButton();
+        menuPage.clickOnAboutButton();
+        Assertions.assertEquals(DriverManager.getDriver().driver.getCurrentUrl(), "https://saucelabs.com/");
+    }
+
+    @Test
+    public void logoutFromMenu()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        MenuPage menuPage = new MenuPage(DriverManager.getDriver().driver);
+        menuPage.clickOnMenuButton();
+        menuPage.clickOnLogoutButton();
+
+        Assertions.assertEquals(DriverManager.getCurrentURL(), "https://www.saucedemo.com/");
+    }
+
+    @Test
+    public void resetAppState()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        HomePage homePage = new HomePage(DriverManager.getDriver().driver);
+        homePage.addProductToCart("Sauce Labs Fleece Jacket");
+        homePage.addProductToCart("Sauce Labs Bike Light");
+
+        MenuPage menuPage = new MenuPage(DriverManager.getDriver().driver);
+        menuPage.clickOnMenuButton();
+        menuPage.clickOnResetStateButton();
+
+        Assertions.assertTrue(homePage.areProductButtonsDefault());
+        Assertions.assertTrue(homePage.correctAddedItems(0));
+    }
+
+}
