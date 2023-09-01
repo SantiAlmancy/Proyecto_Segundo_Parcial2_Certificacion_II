eaf28c5d0ccd79050a99c68ff4e98e291463d8e0
Santiago Becerra <santialmancy@gmail.com>
2023-09-01 14:45:36 -0400

    DEVELOP Testcases Checkout with wrong formulary information created.

--- Parcial_2_CertificacionII/src/test/java/CheckoutInformationTests.java
+++ Parcial_2_CertificacionII/src/test/java/CheckoutInformationTests.java
@@ -71,5 +71,113 @@ public class CheckoutInformationTests extends BaseTest
 
         Assertions.assertTrue(yourCartPage.isCheckOutButtonDisplayed());
     }
-    
+    @Test
+    public void verifyCheckOutWithEmptyFirstName()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        HomePage homePage = new HomePage(DriverManager.getDriver().driver);
+        homePage.addProductToCart("Sauce Labs Fleece Jacket");
+        homePage.addProductToCart("Sauce Labs Bike Light");
+
+        homePage.clickOnShoppingCartButton();
+
+        YourCartPage yourCartPage = new YourCartPage(DriverManager.getDriver().driver);
+        yourCartPage.clickOnCheckoutButton();
+
+        CheckoutInformationPage checkoutPage = new CheckoutInformationPage(DriverManager.getDriver().driver);
+        checkoutPage.setFistNameTextBox("");
+        checkoutPage.setLastNameTextBox("Lastname");
+        checkoutPage.setPostalCodeTextBox("1233");
+        checkoutPage.clickOnContinueButton();
+
+        Assertions.assertTrue(checkoutPage.isErrorTextDisplayed("Error: First Name is required"));
+    }
+
+    @Test
+    public void verifyCheckOutWithEmptyLastName()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        HomePage homePage = new HomePage(DriverManager.getDriver().driver);
+        homePage.addProductToCart("Sauce Labs Fleece Jacket");
+        homePage.addProductToCart("Sauce Labs Bike Light");
+
+        homePage.clickOnShoppingCartButton();
+
+        YourCartPage yourCartPage = new YourCartPage(DriverManager.getDriver().driver);
+        yourCartPage.clickOnCheckoutButton();
+
+        CheckoutInformationPage checkoutPage = new CheckoutInformationPage(DriverManager.getDriver().driver);
+        checkoutPage.setFistNameTextBox("Firstname");
+        checkoutPage.setLastNameTextBox("");
+        checkoutPage.setPostalCodeTextBox("1233");
+        checkoutPage.clickOnContinueButton();
+
+        Assertions.assertTrue(checkoutPage.isErrorTextDisplayed("Error: Last Name is required"));
+    }
+
+    @Test
+    public void verifyCheckOutWithEmptyPostalCodeAndSelectedProducts()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        HomePage homePage = new HomePage(DriverManager.getDriver().driver);
+        homePage.addProductToCart("Sauce Labs Fleece Jacket");
+        homePage.addProductToCart("Sauce Labs Bike Light");
+
+        homePage.clickOnShoppingCartButton();
+
+        YourCartPage yourCartPage = new YourCartPage(DriverManager.getDriver().driver);
+        Assertions.assertTrue(yourCartPage.isProductDisplayed("Sauce Labs Fleece Jacket"));
+        Assertions.assertTrue(yourCartPage.isProductDisplayed("Sauce Labs Bike Light"));
+
+        yourCartPage.clickOnCheckoutButton();
+
+        CheckoutInformationPage checkoutPage = new CheckoutInformationPage(DriverManager.getDriver().driver);
+        checkoutPage.setFistNameTextBox("Firstname");
+        checkoutPage.setLastNameTextBox("Lastname");
+        checkoutPage.setPostalCodeTextBox("");
+        checkoutPage.clickOnContinueButton();
+
+        Assertions.assertTrue(checkoutPage.isErrorTextDisplayed("Error: Postal Code is required"));
+    }
+
+    @Test
+    public void verifyCheckOutWithInvalidPostalCodeAndSelectedProducts()
+    {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        HomePage homePage = new HomePage(DriverManager.getDriver().driver);
+        homePage.addProductToCart("Sauce Labs Fleece Jacket");
+        homePage.addProductToCart("Sauce Labs Bike Light");
+
+        homePage.clickOnShoppingCartButton();
+
+        YourCartPage yourCartPage = new YourCartPage(DriverManager.getDriver().driver);
+        Assertions.assertTrue(yourCartPage.isProductDisplayed("Sauce Labs Fleece Jacket"));
+        Assertions.assertTrue(yourCartPage.isProductDisplayed("Sauce Labs Bike Light"));
+
+        yourCartPage.clickOnCheckoutButton();
+
+        CheckoutInformationPage checkoutPage = new CheckoutInformationPage(DriverManager.getDriver().driver);
+        checkoutPage.setFistNameTextBox("Firstname");
+        checkoutPage.setLastNameTextBox("Lastname");
+        checkoutPage.setPostalCodeTextBox("aabcd");
+        checkoutPage.clickOnContinueButton();
+
+        Assertions.assertFalse(checkoutPage.isErrorTextDisplayed("Error: Postal Code is required"));
+    }
 }
